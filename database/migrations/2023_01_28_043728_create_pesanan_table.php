6a852fca904f64c7193a6df1841ee331df31bf5d
tej0kk <eldykurniadi@gmail.com>
2023-01-28 12:02:07 +0700

    struktur tabel sistem

---
+++ database/migrations/2023_01_28_043728_create_pesanan_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('pelanggan_id');
+            $table->bigInteger('keranjang_id');
+            $table->bigInteger('bank_id');
+            $table->text('catatan');
+            $table->bigInteger('pengiriman_id');
+            $table->enum('metode_bayar', ['transfer_bank','cod']);
+            $table->decimal('total_ongkir');
+            $table->decimal('total_tagihan');
+            $table->enum('status_bayar', ['unpaid','paid']);
+            $table->dateTime('tgl_bayar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanan');
+    }
+};
